c83a5b687cf1f2e8c4dadb896b60bc4e5ada9869
Punl.X <kwangthirakul@gmail.com>
2025-03-17 18:15:08 +0700

    fix ssr

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,22 +3,22 @@ import { build } from 'esbuild';
 async function runBuild() {
   // 1) bundle/minify ฝั่ง client => dist/index.js
   await build({
-    entryPoints: ['dist/client/index.js'], // <-- ผลจากการ tsc (client)
+    entryPoints: ['dist/client/index.js'],
     outfile: 'dist/index.js',
     bundle: true,
     minify: true,
     platform: 'browser',
-    format: 'esm'
+    format: 'esm',
   });
 
   // 2) bundle/minify ฝั่ง server => dist/server.js
   await build({
-    entryPoints: ['dist/server/index.js'], // <-- ผลจากการ tsc (server)
+    entryPoints: ['dist/server/index.js'],
     outfile: 'dist/server.js',
     bundle: true,
     minify: true,
     platform: 'node',
-    format: 'esm'
+    format: 'esm',
   });
 }
 
